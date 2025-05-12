bed2f3085ade57bd1e5215e94d148e371764cad1
piyushmani23 <piyush99.m@gmail.com>
2025-05-12 10:07:18 +0530

    Function add a testimonials

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -4,26 +4,59 @@ import client from '../assets/client.jpg'
 import { ImQuotesLeft } from "react-icons/im";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaLongArrowAltLeft } from "react-icons/fa";
+// import people from '../Components/data.jsx'
+import people from "../Components/reviews.jsx"
+import { useState } from 'react';
 
 
 
 
 function Testimonial() {
+  const [index, setIndex] = useState(0);
+  const { name, job, imgage, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1){
+      return 0;
+    }
+    if (number < 0){
+      return people.length - 1;
+    }
+    return number;
+  }
+
+
+  const nextPerson = () =>{
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    })
+  }
+
+  const previousPerson = () =>{
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    })
+
+  }
+
+
   return (
     <div className='testimonial'>
       <h3>Testimonial</h3>
 
       <div className="testimonial_card">
-      <img src={client} alt="" />
-      <h3>Samantha Jonas</h3>
-      <p>It is a long established fact that a reader will bedistracted by the readable cIt is a long established factthat a reader will be distracted by the readable </p>
-      <h2><ImQuotesLeft />
-      </h2>
+      <img src={imgage} alt="" />
+      <h3>{name}</h3>
+      <p>{text} </p>
+      <h2>  <ImQuotesLeft /> </h2>
+      <h2> {job} </h2>
       </div>
 
       <div className="testimonial_button">
-        <button><FaLongArrowAltLeft className='testimonial_left' /> </button>
-        <button> <FaLongArrowAltRight className='testimonial_right'/> </button>
+        <button onClick={previousPerson}><FaLongArrowAltLeft className='testimonial_left' /> </button>
+        <button onClick={nextPerson}> <FaLongArrowAltRight className='testimonial_right'/> </button>
       </div>
       
       </div>
---
+++ src/Components/data.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import t1 from "../assets/b1.jpg"
+import t2 from "../assets/b2.jpg"
+import t3 from "../assets/b1.jpg"
+import t4 from "../assets/b1.jpg"
+
+function data() {
+    const data = [ {
+        id : 1,
+        name : "Susan sumit",
+        job : " Web developer",
+        imgage : t1,
+        text : "It is a long established fact that a reader will bedistracted by the readable cIt is a long established factthat a reader will be distracted by the readable"
+
+    },
+    {
+        id : 2,
+        name : "Piyush",
+        job : " Software developer",
+        imgage : t2,
+        text : "It is a long established fact that a reader will bedistracted by the readable cIt is a long established factthat a reader will be distracted by the readable"
+
+    },
+    {
+        id : 3,
+        name : "Prakash Raj",
+        job : "Devos",
+        imgage : t3,
+        text : "It is a long established fact that a reader will bedistracted by the readable cIt is a long established factthat a reader will be distracted by the readable"
+
+    },
+    {
+        id : 4,
+        name : "Anu",
+        job : "testr",
+        imgage : t4,
+        text : "It is a long established fact that a reader will bedistracted by the readable cIt is a long established factthat a reader will be distracted by the readable"
+
+    },
+
+];
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default data;
